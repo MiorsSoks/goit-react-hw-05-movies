7881a40f27d6f2d08f6160b8c97425b0ba701b44
Myroslav Sokolov <mirossoks@gmail.com>
2022-04-15 21:06:18 +0300

    v 1.0

--- src/components/API/GetFilms.jsx
+++ src/components/API/GetFilms.jsx
@@ -1,9 +1,20 @@
-import axios from 'axios'
+// import axios from 'axios'
+// import PropTypes from 'prop-types';
 
-export default function GetFilms() {}
-  axios({
-  url: 'https://api.themoviedb.org/3//trending/{media_type}/{time_window}?api_key=b5d359bd4cfe918eaffdc43e2c19ce7b',
-})
-  .then(function (response) {
-    console.log(response)
-  });
+// export default function GetFilms(setData) {
+
+//  axios
+//       .get(
+//         'https://api.themoviedb.org/3//trending/movie/week?api_key=b5d359bd4cfe918eaffdc43e2c19ce7b'
+//       )
+//       .catch(error => console.log('Error', error.message))
+//       .then(response => {
+//         const results = response.data.results;
+//         setData(results)
+//       })
+// }
+
+// GetFilms.propTypes = {
+//   setData: PropTypes.func.isRequired,
+
+// };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,26 @@
-import GetFilms from '../API'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 export default function Home() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.themoviedb.org/3//trending/movie/week?api_key=b5d359bd4cfe918eaffdc43e2c19ce7b'
+      )
+      .catch(error => console.log('Error', error.message))
+      .then(response => {
+        const results = response.data.results;
+        setData(results);
+      });
+  }, []);
+
+  console.log(data);
   return (
     <>
       <h2>Tranding today</h2>
-      <p>{GetFilms}</p>
-        <ul></ul>
+      <ul>{data.map(title => { return (<li key={title.title}><Link to={title.title}>{title.title}</Link></li>)})}</ul>
     </>
   );
 }
